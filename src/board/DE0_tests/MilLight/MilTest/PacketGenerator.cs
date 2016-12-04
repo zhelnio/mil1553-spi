f0e9e29c5a9fdb8b9897e6720bfb39d97d3d1964
Stanislav Zhelnio <zhelniosl@yandex.ru>
2016-12-04 14:29:02 +0300

    refactoring

---
+++ src/board/DE0_tests/MilLight/MilTest/PacketGenerator.cs
@@ -0,0 +1,55 @@
+﻿/* MIL-STD-1553 <-> SPI bridge
+ * Copyright(c) 2016 Stanislav Zhelnio
+ * Licensed under the MIT license (MIT)
+ * https://github.com/zhelnio/mil1553-spi
+ */
+
+using MilTest.MilLight.ServiceProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilTest
+{
+    static class PacketGenerator
+    {
+        private static Random rnd = new Random();
+
+        public static List<IMilFrame> randomPacket(int size)
+        {
+            if (size == 0)
+                throw new ArgumentException("packet size should be greater then 0");
+
+            List<IMilFrame> result = new List<IMilFrame>();
+
+            //header
+            result.Add(new MilFrame() { Header = MilType.WSERV, Data = (ushort)rnd.Next() });
+
+            //body
+            for (int i = 1; i < size; i++)
+                result.Add(new MilFrame() { Header = MilType.WDATA, Data = (ushort)rnd.Next() });
+
+            return result;
+        }
+
+        public static List<IMilFrame> randomPacket()
+        {
+            const int maxPacketSize = 30;
+            return randomPacket(rnd.Next(1, maxPacketSize));
+        }
+
+        public static List<IMilFrame> fixedPacket()
+        {
+            return new List<IMilFrame>()
+            {
+                new MilFrame() { Data = 0x0001, Header = MilType.WSERV },
+                new MilFrame() { Data = 0x0002 },
+                new MilFrame() { Data = 0xAB45 },
+                new MilFrame() { Data = 0xFFA1 }
+            };
+        }
+    }
+
+}
